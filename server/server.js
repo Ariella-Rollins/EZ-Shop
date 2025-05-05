3a267b775d4b13e0f6533c4fb010c407e86a3978
mustafa2590 <mujtaba.mustafazada@gmail.com>
2025-05-05 15:03:02 -0700

    Update server.js

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ import cookieParser from "cookie-parser";
 
 const app = express();
 
-app.use(express.json(), cors({ origin: 'http://localhost:5173', credentials: true }));
+app.use(express.json(), cors({ origin: 'http://localhost:5174', credentials: true }));
 app.use(express.json())
 app.use(cookieParser())
 app.use("/v1/user", userRouter);

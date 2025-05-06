babe1f0342fb584977d64b6cfedbae64ee028cb5
mustafa2590 <mujtaba.mustafazada@gmail.com>
2025-05-06 00:02:35 -0700

    Update product.routes.js

--- server/routes/product.routes.js
+++ server/routes/product.routes.js
@@ -11,6 +11,11 @@ productRouter.route('/')
     .post( createProduct )
     .put (updateProduct)
     .delete (deleteProduct)
-
+// filter or search for products based on category
+productRouter.route(/category/:category)
+    .get(getProductsByCategory);
+//decrease number of stock after purchase happen
+productRouter.route('/:id')
+    .patch(updateStock)
 
 export default productRouter
